91796af1b4167218e05edb4683abff26e9bf68d4
Jack Walker <personal@jackwalker.co.nz>
2024-02-14 22:00:08 +1000

    Add change to branches

--- SpeckleAutomateDotnetExample/Branches.cs
+++ SpeckleAutomateDotnetExample/Branches.cs
@@ -1,6 +1,12 @@
+
+
+using System.Text.Json.Serialization;
+
 public class Branches 
 {
     public string ElementAssignment {get;set;}
     public string Materials {get;set;}
     public string Elements {get;set;}
+    [JsonConstructor]
+    public Branches(){}
 }
